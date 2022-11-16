32b24b0c177c9095004ad05a3435508e18f23682
Arlaine Sanchez <amaesanchez@gmail.com>
2022-11-15 21:45:46 -0500

    add docstrings and reformat

--- app.js
+++ app.js
@@ -1,41 +1,61 @@
 "use strict";
 
-const baseURL = "http://api.giphy.com/v1/gifs/search"
+const baseURL = "http://api.giphy.com/v1/gifs/search";
 const APIKey = "RjDukV642qX8mGf72Qt7SwTLhJ4pi4fQ";
-const gifArrLength = 50;
+const gifListLength = 50;
 
-/**  */
-async function searchGiphy(evt) {
-  evt.preventDefault();
+/** makes get request for giphy URL with user input as
+ * query string
+ */
+
+async function searchGiphy() {
   let searchResult = $("#input").val();
 
   const response = await axios.get(baseURL, {
     params: { q: searchResult, api_key: APIKey },
   });
-  let gifList = response.data.data
-  let gifData = randomImg(gifList);
-  addGiphy(gifData);
+
+  return response.data.data;
 }
 
-
-/**  */
-function randomImg(imgArr) {
-  return imgArr[Math.floor(Math.random() * gifArrLength)];
+/** gets random gif object from gif array inside the GET object */
+function randomImg(gifList) {
+  return gifList[Math.floor(Math.random() * gifListLength)];
 }
 
-function addGiphy(img) {
-  let gifURL = img.images.downsized.url;
-
-  let $gifImg = $("<img>").attr("src", gifURL);
+/** appends image with gif url to the image container div in body */
+function addGiphy(gif) {
+  const gifURL = gif.images.downsized.url;
+  const $gifImg = $("<img>").attr("src", gifURL);
 
   $("#img-container").append($gifImg);
 }
 
+/** removes all images in image container div */
+function removeImgs() {
+  $("#img-container").empty();
+}
+
+/** CONSTRUCTOR function:
+ * revents refresh bc submit button default behavior is
+ * to refresh page
+ *
+ * invokes searchGiphy, randomImg, and addGiphy to append randomized img
+ * to DOM based on user input
+ */
+
+async function displayGiphy(evt) {
+  evt.preventDefault();
+  let gifList = await searchGiphy();
+  let gifData = randomImg(gifList);
+  addGiphy(gifData);
+}
+
+/** event listener for submit button */
+$("#search").on("click", displayGiphy);
+
+/** event listener for removing all images */
+$("#remove").on("click", remove);
+
 // submit buttons default behavior is to refresh page, must add
 // preventdefault to ftn
-
-$("#search").on("click", searchGiphy);
-
-$("#remove").on("click", function (evt) {
-  $("#img-container").empty();
-});
