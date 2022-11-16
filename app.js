6420fb75943526365b0ff2ff02d24c612db6d108
Arlaine Sanchez <amaesanchez@gmail.com>
2022-11-15 20:51:03 -0500

    add gifs

--- app.js
+++ app.js
@@ -1,12 +1,41 @@
-'use strict';
+"use strict";
 
-const $submitButton = $('#search');
-const $removeButton = $('#remove');
+const baseURL = "http://api.giphy.com/v1/gifs/search"
+const APIKey = "RjDukV642qX8mGf72Qt7SwTLhJ4pi4fQ";
+const gifArrLength = 50;
 
-$submitButton.submit(getGiphy);
+/**  */
+async function searchGiphy(evt) {
+  evt.preventDefault();
+  let searchResult = $("#input").val();
 
-async function getGiphy() {
-    let sResult = $('input').val();
-    let searchResult = await axios.get('http://api.giphy.com/v1/gifs/search?', { params: { sResult } })
-    console.log(searchResult);
+  const response = await axios.get(baseURL, {
+    params: { q: searchResult, api_key: APIKey },
+  });
+  let gifList = response.data.data
+  let gifData = randomImg(gifList);
+  addGiphy(gifData);
 }
+
+
+/**  */
+function randomImg(imgArr) {
+  return imgArr[Math.floor(Math.random() * gifArrLength)];
+}
+
+function addGiphy(img) {
+  let gifURL = img.images.downsized.url;
+
+  let $gifImg = $("<img>").attr("src", gifURL);
+
+  $("#img-container").append($gifImg);
+}
+
+// submit buttons default behavior is to refresh page, must add
+// preventdefault to ftn
+
+$("#search").on("click", searchGiphy);
+
+$("#remove").on("click", function (evt) {
+  $("#img-container").empty();
+});
